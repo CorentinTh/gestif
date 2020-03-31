92ca9a97a821b09cc426b9b7454cf96bd6846a95
DelileHugo <hugo.delile@insa-lyon.fr>
2020-03-31 12:01:27 +0200

    getStudentById

--- src/services/students.service.js
+++ src/services/students.service.js
@@ -4,7 +4,12 @@ const getTenStudents = (offSet) => {
     return db.prepare('SELECT * FROM students LIMIT 10 OFFSET ' + (offSet * 10)).all();
 };
 
+const getStudentById = (id) => {
+    return db.prepare('SELECT * FROM students Where Id = '+id).get();
+};
+
 module.exports = {
-    getTenStudents
+    getTenStudents,
+    getStudentById
 };
 
